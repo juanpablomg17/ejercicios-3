999b9220e7cbbfc2a2fa7df5c4eccec05422cb67
unknown <juanpablomg17@gmail.com>
2020-03-02 12:27:42 -0500

    ejercicio10-validado

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,34 +1,47 @@
 def ejercicio10():
     numeros = []
     numeros2 = []
+    
+
 
     n = int(input("digite el tope de la lista"))
-
-    for i in range(n):
-        i = int(input("dame un número: "))
-        numeros.append(i)
+    if (n > 0):
+        for i in range(n):
+            i = int(input("dame un número: "))
+            numeros.append(i)
     
-    listaP = numeros
-    print(f"la lista ingresada fue: {listaP}")
+        listaP = numeros
+        print(f"la lista ingresada fue: {listaP}")
 
-    numeros.pop(-1)
+        numeros.pop(-1)
 
-    j = int(input("digite el tope de la otra lista: "))
-    numeros2 = numeros
+        j = int(input("digite el tope de la otra lista: "))
+        numeros2 = numeros
     
-    for i in range(j):
-        i = int(input("dame otro número: "))
-        numeros2.append(i)
+        for i in range(j):
+            i = int(input("dame otro número: "))
+            numeros2.append(i)
 
  
-    print(f"la nueva lista fue: {numeros2}")
+        print(f"la nueva lista fue: {numeros2}")
+
+    else:
+        print("la cantidad introducida, debe ser mayor que cero")
 
     #hola mundo
    
+
+def validar():
+    correcto = False
+    while (not correcto):
+        try:
+            ejercicio10()
+            correcto = True
+
+
+        except:
+            print("Error, el dato debe ser de tipo numérico (entero)")
    
  
 
-    
-
-   
-ejercicio10()
+validar()
