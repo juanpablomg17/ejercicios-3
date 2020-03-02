8d6153761af5d83d732c58ec608b1e115d5d3470
unknown <juanpablomg17@gmail.com>
2020-03-02 13:14:11 -0500

    falta-el-8

--- ejercicio8.py
+++ ejercicio8.py
@@ -20,43 +20,65 @@ def serieFibonacci():
         f"Los primeros {n} numeros de la serie fibonacci son: {numerosfibonacci}")
 
 
+# def Nprimos():
+#     primos = []
+#     dato = int(input("digite el tope o tamaño de su lista"))
+#     j = 2
+#     while j < dato:
+#         creciente = 2
+#         esprimo = True
+#         while esprimo and creciente < j:
+#             if j % creciente == 0:
+#                 esprimo = False
+#             else:
+#                 creciente = creciente + 1
+#             if esprimo:
+#                 primos.append(j)
+#                 j = j +1
+
+#     print(f"Los primeros {dato} numeros primos son: {primos}")
+
 def Nprimos():
+    comp = True
     primos = []
     dato = int(input("digite el tope o tamaño de su lista"))
-    j = 2
-    while j < dato:
-        creciente = 2
-        esprimo = True
-        while esprimo and creciente < j:
-            if j % creciente == 0:
-                esprimo = False
-            else:
-                creciente = creciente + 1
-            if esprimo:
-                primos.append(j)
-        j = j + 1
 
-    print(f"Los primeros {dato} numeros primos son: {primos}")
+    if dato > 0:
+        comp = False
+        i = 2
+        while i < dato:
+            creciente = 2
+            esPrimo = True
+
+            while esPrimo and creciente < i:
+                if i % creciente == 0:
+                    esPrimo = False
+                else:
+                    creciente += 1
+            if esPrimo:
+                primos.append(i)
+            i+=1
+        print(f"Los primeros {dato} numeros primos son: {primos}")
 
 
 def correr():
-    
-   correcto = False
-   while(not correcto):
-       try:
+
+    correcto = False
+    while(not correcto):
+        try:
             print("Menú. \n 1. imprimir una lista de la serie fibonacci hasta n numeros \n 2. imprimir una lista de n numeros primos ")
             op = int(input(""))
 
             if (op == 1):
                 serieFibonacci()
-            if (op ==2):
+            if (op == 2):
                 Nprimos()
             else:
-                 print("Opción inválida")
-            
+                print("Opción inválida")
+
             correcto = True
-       except ValueError:
-          print("Error, la cantidad ingresada debe ser de tipo numética")
+        except ValueError:
+            print("Error, la cantidad ingresada debe ser de tipo numética")
 
 
 correr()
